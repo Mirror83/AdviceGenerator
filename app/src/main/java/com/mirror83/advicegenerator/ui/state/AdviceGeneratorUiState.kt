c070f996b51769959da4b775fffac7e930467167
Mirror83 <glenochieng045@gmail.com>
2025-08-13 12:19:13 +0300

    refactor: modify ui state to hold Advice object instead of the advice string

--- app/src/main/java/com/mirror83/advicegenerator/ui/state/AdviceGeneratorUiState.kt
+++ app/src/main/java/com/mirror83/advicegenerator/ui/state/AdviceGeneratorUiState.kt
@@ -1,8 +1,10 @@
 package com.mirror83.advicegenerator.ui.state
 
+import com.mirror83.advicegenerator.network.Advice
+
 
 sealed interface AdviceGeneratorUiState {
-    data class Success(val advice: String): AdviceGeneratorUiState
+    data class Success(val advice: Advice): AdviceGeneratorUiState
     object Error: AdviceGeneratorUiState
     object Loading: AdviceGeneratorUiState
 }
