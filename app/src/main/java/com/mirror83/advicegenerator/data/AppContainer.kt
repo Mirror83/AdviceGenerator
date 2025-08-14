f265e5a913c51c4792cdd3c93c1e5de856f36483
Mirror83 <glenochieng045@gmail.com>
2025-08-14 21:44:59 +0300

    chore: remove unused import

--- app/src/main/java/com/mirror83/advicegenerator/data/AppContainer.kt
+++ app/src/main/java/com/mirror83/advicegenerator/data/AppContainer.kt
@@ -7,7 +7,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlin.getValue
 
 interface AppContainer {
     val adviceRepository: AdviceRepository
